0428252fe6d765f47f23d54cb03a5a8ab5012c03
Avraham Natan <avinat123@gmail.com>
2019-07-15 21:39:58 +0300

    Added JavaDoc - part 1

--- src/server/IMSServer.java
+++ src/server/IMSServer.java
@@ -8,6 +8,21 @@ import java.util.Set;
 
 import server.networking.ClientHandler;
 
+/**
+ * The Instant Message Service (IMS) Server class.
+ * 
+ * Extends Thread by having a run method, where it's main loop runs.
+ * Inside this loop the server listens for new connections, and for
+ * every new accepted connection it either creates a new or uses an
+ * already created {@link ClientHandler}, that manages server side
+ * communication for the client that the connection originated from.
+ * 
+ * Also provides handful of functions to be used by ClientHandlers
+ * to manage the state of registered clients.
+ * 
+ * @author Avi
+ *
+ */
 public class IMSServer extends Thread {
 	
 	private Set<ClientHandler> registeredClients = new HashSet<>();
@@ -15,6 +30,12 @@ public class IMSServer extends Thread {
 	private ServerSocket server;
 	private int port = 8877;
 	
+	/**
+	 * Constructs this server with the given port as the parameter,
+	 * the server will listen to the given port.
+	 * 
+	 * @param port - The port that the server will listen on.
+	 */
 	public IMSServer(int port) {
 		this.port = port;
 		try {
@@ -24,6 +45,10 @@ public class IMSServer extends Thread {
 		}
 	}
 	
+	/**
+	 * Constructs this server with no port as parameter. The server
+	 * will listen on port 8877.
+	 */
 	public IMSServer() {
 		try {
 			server = new ServerSocket(port);
@@ -32,6 +57,16 @@ public class IMSServer extends Thread {
 		}
 	}
 	
+	
+	/**
+	 * Main loop.
+	 * 
+	 * The server listens to incoming connections and for every
+	 * connection it constructs a {@link ClientHandler} that tries
+	 * to establish more permanent connection using the method
+	 * {@link ClientHandler#handshake()}.
+	 * 
+	 */
 	@Override
 	public void run() {
 		Socket clientSocket;
@@ -46,12 +81,25 @@ public class IMSServer extends Thread {
 		}
 	}
 	
-	
+	/**
+	 * Removes a registered {@link ClientHandler} from the registered clients database.
+	 * 
+	 * @param c - The client to remove.
+	 * 
+	 * <dt>Precondition: <dd> The client c exists in the registered clients.
+	 */
 	public void removeClient(ClientHandler c) {
 		System.out.println("removing");
 		registeredClients.remove(c);
 	}
 	
+	/**
+	 * Checks to see if the server has a registered {@link ClientHandler} with the
+	 * same username as the given username.
+	 * 
+	 * @param username - The given username.
+	 * @return True if the server contains such username.
+	 */
 	public boolean hasUserName(String username) {
 		for(ClientHandler ch : registeredClients) {
 			if(ch.getUsername().equals(username)) return true;
@@ -59,6 +107,13 @@ public class IMSServer extends Thread {
 		return false;
 	}
 
+	/**
+	 * Checks to see if the server has a registered {@link ClientHandler} with the
+	 * same email address as the given email.
+	 * 
+	 * @param email - The given email.
+	 * @return True if the server contains such email.
+	 */
 	public boolean hasEmail(String email) {
 		for(ClientHandler ch : registeredClients) {
 			if(ch.getEmail().equals(email)) return true;
@@ -66,10 +121,26 @@ public class IMSServer extends Thread {
 		return false;
 	}
 	
+	/**
+	 * Registers a new {@link ClientHandler} to this server,
+	 * namely adds it to the registered client handlers.
+	 * 
+	 * @param clientHandler - the client handler to add.
+	 * 
+	 * <dt>Precondition: <dd> There are no client handlers with the same
+	 * username or email address as the one to be registered.
+	 */
 	public void addToClients(ClientHandler clientHandler) {
 		registeredClients.add(clientHandler);
 	}
 	
+	/**
+	 * Returns the registered {@link ClientHandler} that has the same
+	 * username, or null if there is no such ClientHandler.
+	 * 
+	 * @param username - The username that the desired ClientHandler has.
+	 * @return The ClientHandler that has the desired username, or null.
+	 */
 	public ClientHandler getClientHandler(String username) {
 		for(ClientHandler ch : registeredClients) {
 			if(ch.getUsername().equals(username)) {
@@ -79,7 +150,12 @@ public class IMSServer extends Thread {
 		return null;
 	}
 
-	
+	/**
+	 * Entry point for the server. The process begins from here.
+	 * It constructs an instance of the IMS server and runs it.
+	 * 
+	 * @param args - Command line arguments.
+	 */
 	public static void main(String[] args) {
 
 		System.out.println("Hello IMS Server!");
--- src/server/protocol/IMSProtocol.java
+++ src/server/protocol/IMSProtocol.java
@@ -1,10 +1,50 @@
 package server.protocol;
 
+
+/**
+ * The Instant Message Service (IMS) Protocol presents 2 static methods,
+ * one for converting an array of bytes to an array of strings,
+ * and one for converting an array of strings to an array of bytes,
+ * both of which represent a message from the IMS client to the server
+ * and vice versa.
+ * 
+ * The protocol makes sure that strings containing a newline or return
+ * carriage characters ('\n', '\r') are replaced with a special 
+ * "NEWLINEHOLDER" character, in order to prevent a situation where
+ * part of the communicated message will not be sent because it
+ * contained a new line character - that makes the input stream to stop
+ * reading data.
+ * 
+ * The protocol uses byte -55 (called DELIM) to separate messages and
+ * byte -57 (called NEWLINEHOLDER) to indicate newline characters
+ * within a message. Every byte array ends with the newline '\n' byte.
+ * 
+ * For example, a message that represents a correspondence message from
+ * user A to user B will be represented as such:
+ * 
+ * As strings: { "MESSAGE", "A", "Hello,\r\nWorld!" }
+ * As byte array:
+ * { -55, 77, 69, 83, 83, 65, 71, 69,
+ *   -55, 65,
+ *   -55, 72, 101, 108, 108, 111, 44, -57, -57, 87, 111, 114, 108, 100, 33, 10 }
+ *   
+ * 
+ * @author Avi
+ *
+ */
 public class IMSProtocol {
 	
 	private static final byte DELIM = (byte)-55;
 	private static final byte NEWLINEHOLDER = (byte)-57;
 	
+	/**
+	 * Takes an array of bytes that represent a message and converts them
+	 * to an array of strings, that can be converted back to the same
+	 * array of bytes with this protocol.
+	 * 
+	 * @param bytes - The byte array that represents the message.
+	 * @return A string array that represents the message.
+	 */
 	public static String[] bytesToMessage(byte[] bytes) {
 		int stringsCount = 0;
 		for(int i = 0; i < bytes.length; i++) {
@@ -37,6 +77,14 @@ public class IMSProtocol {
 		return message;
 	}
 	
+	/**
+	 * Takes an array of strings that represent a message and converts
+	 * them to an array of bytes, that can be converted back to the same
+	 * array of strings with this protocol.
+	 * 
+	 * @param message - The string array that represents the message.
+	 * @return A byte array that represents the message.
+	 */
 	public static byte[] messageToBytes(String[] message) {
 		int delimCount = message.length;
 		int messageLength = 0;
